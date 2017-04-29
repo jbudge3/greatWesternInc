455a9241bd74a1f25e41b94e8517ea491a9ed61a
Jake Budge <jake.budge@me.com>
2017-04-29 17:24:49 -0600

    Completed styling for mobile header

--- js/controllers/headerCtrl.js
+++ js/controllers/headerCtrl.js
@@ -1,3 +1,10 @@
 angular.module('greatWestern').controller('headerCtrl', function($scope) {
+    var $ = angular.element;
+    $scope.menuOpen = false;
+
+    $scope.showMenu = function() {
+        $scope.menuOpen = !$scope.menuOpen;
+        $('.header__container-nav').slideToggle();
+    }
 
 });
