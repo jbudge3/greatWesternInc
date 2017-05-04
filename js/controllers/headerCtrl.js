719671b17f59695de074210e818f0972115f73d2
Jake Budge <jake.budge@me.com>
2017-05-03 20:00:54 -0600

    Updated menu UI

--- js/controllers/headerCtrl.js
+++ js/controllers/headerCtrl.js
@@ -7,4 +7,30 @@ angular.module('greatWestern').controller('headerCtrl', function($scope) {
         $('.header__container-nav').slideToggle();
     }
 
+    $scope.positionTriangle = function() {
+        var currentPath = window.location.pathname;
+        var targetLink = '';
+
+        switch(currentPath) {
+            case '/':
+                targetLink = '.js-home';
+                break;
+            default:
+                //Have a party!
+        }
+
+        var posLeft = $(targetLink).position().left - 3;
+        var halfWidth = $(targetLink).outerWidth() / 2;
+
+        $('.header__whiteTriangle').animate({
+            left: (posLeft + halfWidth) + "px"
+        }, 100);
+    }
+
+    $scope.positionTriangle();
+
+    $(window).resize(function() {
+        $scope.positionTriangle();
+    });
+
 });
