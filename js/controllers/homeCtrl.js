cc37bed8abd22a096a762d3f04775d219e0482fa
Jake Budge <jake.budge@me.com>
2017-05-05 20:55:27 -0600

    Added home content with images and carousel

--- js/controllers/homeCtrl.js
+++ js/controllers/homeCtrl.js
@@ -1,3 +1,12 @@
 angular.module('greatWestern').controller('homeCtrl', function($scope) {
-    
+    var $ = angular.element;
+
+    $(document).ready(function(){
+        $('.testing').slick({
+        dots: true,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplayspeed: 2000
+        });
+    });
 });
