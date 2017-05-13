d1a59461dba8b738784fe4c16d26406501709bc8
Jake Budge <jake.budge@me.com>
2017-05-13 09:57:36 -0600

    Added styling to carousel

--- js/controllers/homeCtrl.js
+++ js/controllers/homeCtrl.js
@@ -2,11 +2,13 @@ angular.module('greatWestern').controller('homeCtrl', function($scope) {
     var $ = angular.element;
 
     $(document).ready(function(){
-        $('.testing').slick({
+        $('.homeContainer__carousel').slick({
+        arrows: false,
         dots: true,
         slidesToShow: 1,
         autoplay: true,
-        autoplayspeed: 2000
+        autoplayspeed: 2000,
+        mobileFirst: true
         });
     });
 });
